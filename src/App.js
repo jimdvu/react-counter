af2c490d273777c493b8424f021fbda9381875b8
Jimmy <jimdvu@gmail.com>
2021-08-31 16:49:36 -0500

    made a react counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
+}
+
+class Counter extends Component{
+
+  state={
+    count: 0
+  };
+
+  handleClick = () => {
+    this.setState(( {count}) => ({
+      count: count + 1
+    }));
+  };
+
+  render(){
+    return <button onClick = {this.handleClick}>{this.state.count}</button>;
+  }
 }
 
 export default App;
